2ece7f45a94b702a1aa9590a78c66f592a446bb5
fsantini <francesco.santini@gmail.com>
2016-07-29 21:42:42 +0200

    PageMod support; pagemod on android

--- data/scanResults.js
+++ data/scanResults.js
@@ -20,58 +20,72 @@ function setShipInfo($objDiv, status)
   }
 }
 
-
-$("div.s-item-container").each(function(i) {
-  var $thisObject = $(this);
-  var lnk = $thisObject.find("a.s-access-detail-page").attr("href");
-  
-  if (lnk == null) return;
-  //console.log(lnk);
-  if (lnk.startsWith("/")) // relative url
-  {
-    lnk = window.location.protocol + "//" + window.location.host + lnk;
-  }
-  
-  
-  setShipInfo($thisObject, SHIPMENT_STATUS.UNDEFINED);
+function runScan()
+{
+  //console.log("Running scan");
+  $("div.s-item-container").each(function(i) {
+    var $thisObject = $(this);
     
-  $.get(lnk, function(data) {
+    if ($thisObject.prop("amazon-delivery-addon-checked") == true)
+    {
+      //console.log("div already checked");
+      return;
+    }
+    
+    $thisObject.prop("amazon-delivery-addon-checked", true); // don't check this multiple times
+    
+    var lnk = $thisObject.find("a.s-access-detail-page").attr("href");
+    
+    if (lnk == null) return;
+    //console.log(lnk);
+    if (lnk.startsWith("/")) // relative url
+    {
+      lnk = window.location.protocol + "//" + window.location.host + lnk;
+    }
+    
+    
+    setShipInfo($thisObject, SHIPMENT_STATUS.UNDEFINED);
       
-      if (window.location.host.toLowerCase().indexOf("amazon.com") > -1)
-      {
-        // this is the .com website, it's a bit different than the international sites
-        var deliveryInfo = $(data).find("#fast-track-message");
-        if ($(deliveryInfo).text().indexOf("does not ship") > -1)
+    $.get(lnk, function(data) {
+        
+        if (window.location.host.toLowerCase().indexOf("amazon.com") > -1)
         {
-            //console.log("No delivery");
-            setShipInfo($thisObject, SHIPMENT_STATUS.NO);
-        } else if ($(deliveryInfo).text().indexOf("ships to") > -1)
+          // this is the .com website, it's a bit different than the international sites
+          var deliveryInfo = $(data).find("#fast-track-message");
+          if ($(deliveryInfo).text().indexOf("does not ship") > -1)
+          {
+              //console.log("No delivery");
+              setShipInfo($thisObject, SHIPMENT_STATUS.NO);
+          } else if ($(deliveryInfo).text().indexOf("ships to") > -1)
+          {
+              //console.log("Delivery ok");
+              setShipInfo($thisObject, SHIPMENT_STATUS.YES);
+          }
+        } else
         {
-            //console.log("Delivery ok");
-            setShipInfo($thisObject, SHIPMENT_STATUS.YES);
+          var deliveryInfo = $(data).find("#ddmDeliveryMessage");
+          if (deliveryInfo.length == 0)
+          {
+              //console.log("No deliveryInfo");
+          }
+          else
+          {
+              var noDelivery = $(deliveryInfo).find(".a-color-error");
+              if (noDelivery.length > 0)
+              {
+                  //console.log("No delivery");
+                setShipInfo($thisObject, SHIPMENT_STATUS.NO);
+                  
+              }
+              else
+              {
+                  //console.log("Delivery ok");
+                  setShipInfo($thisObject, SHIPMENT_STATUS.YES);
+              }
+          }
         }
-      } else
-      {
-        var deliveryInfo = $(data).find("#ddmDeliveryMessage");
-        if (deliveryInfo.length == 0)
-        {
-            //console.log("No deliveryInfo");
-        }
-        else
-        {
-            var noDelivery = $(deliveryInfo).find(".a-color-error");
-            if (noDelivery.length > 0)
-            {
-                //console.log("No delivery");
-               setShipInfo($thisObject, SHIPMENT_STATUS.NO);
-                
-            }
-            else
-            {
-                //console.log("Delivery ok");
-                setShipInfo($thisObject, SHIPMENT_STATUS.YES);
-            }
-        }
-      }
+    });
   });
-});
+}
+
+setInterval(runScan, 1000); // run the script every second to catch ajax changes
--- index.js
+++ index.js
@@ -2,19 +2,51 @@ var self = require("sdk/self");
 var buttons = require('sdk/ui/button/action');
 var tabs = require("sdk/tabs");
 
+var pref = require("sdk/simple-prefs");
 
 // pageMod at the moment not working because of ajax: page is not actually reloaded
-//var pageMod = require("sdk/page-mod");
-//
-//pageMod.PageMod({
-//  include: [/.*amazon\.[^/]*\/s\/.*/, /.*amazon\.*\/search\/.*/],
-//  contentScriptWhen: 'ready',
-//  attachTo: 'top',
-//  contentScript: 'console.log("Page matches");',
-//  contentScriptFile: [self.data.url('jquery-3.1.0.min.js'),
-//                        self.data.url('scanResults.js')]
-//});
+var pageMod = require("sdk/page-mod");
 
+function initPageMod()
+{
+  return pageMod.PageMod({
+    include: [/.*amazon\.[^/]*\/s\/.*/, /.*amazon\.*\/search\/.*/],
+    contentScriptWhen: 'ready',
+    attachTo: ['top', 'existing'],
+    contentScript: 'console.log("Page matches");',
+    contentScriptFile: [self.data.url('jquery-3.1.0.min.js'),
+                          self.data.url('scanResults.js')]
+  });
+}
+
+var pmod = null;
+
+
+
+var windows = require("sdk/window/utils");
+var window = windows.getMostRecentBrowserWindow();
+
+var ua = window.navigator.userAgent.toLowerCase();
+var isAndroid = ua.indexOf("android") > -1;
+
+function autoRunChanged()
+{
+  if (pmod)
+  {
+    pmod.destroy();
+    pmod = null;
+  }
+  if (pref.prefs.autoRun || isAndroid)
+    pmod = initPageMod();  
+}
+
+// check if we need to auto-attach the script to amazon search pages
+// under Android there are no buttons, pagemod is the only way
+if (pref.prefs.autoRun || isAndroid)
+  pmod = initPageMod();
+
+// listen when the autoRun property is set
+pref.on("autoRun", autoRunChanged);
 
 var button = buttons.ActionButton({
   id: "amazon-result-scan",
